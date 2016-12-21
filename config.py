4d25380c1074e41d5d18972fb67ced7f59178a2d
Rabih Kodeih <rabih.kodeih@gmail.com>
2016-12-21 10:05:58 +0200

    First Commit

---
+++ config.py
@@ -0,0 +1,27 @@
+'''
+Created on Dec 20, 2016
+
+@author: rabihkodeih
+'''
+
+import os
+
+CLIENT_SECRETS_FILE = "client_secrets.json"
+
+MISSING_CLIENT_SECRETS_MESSAGE = """
+WARNING: Please configure OAuth 2.0
+To make this sample run you will need to populate the client_secrets.json file
+found at:
+   %s
+with information from the {{ Cloud Console }}
+{{ https://cloud.google.com/console }}
+For more information about the client_secrets.json file format, please visit:
+https://developers.google.com/api-client-library/python/guide/aaa_client_secrets
+""" % os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                   CLIENT_SECRETS_FILE))
+YOUTUBE_READ_WRITE_SCOPE = "https://www.googleapis.com/auth/youtube"
+YOUTUBE_API_SERVICE_NAME = "youtube"
+YOUTUBE_API_VERSION = "v3"
+PLAYLIST_ROOT_FOLDER = '/Users/rabihkodeih/Documents/workspace/pytube/downloads/'
+
+
